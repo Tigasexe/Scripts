571b79e62213dfd670d3538d996c8f905f6efe4c
Tigasexe <127435100+Tigasexe@users.noreply.github.com>
2023-04-14 16:36:38 -0300

    Delete Python directory

--- Python/Estudar_Português.py
+++
@@ -1,8 +0,0 @@
-import webbrowser as wb
-
-print('-----'*6)
-print('        Vai estudar!!')
-print('-----'*6)
-
-driver = wb.Mozilla()
-wb.open('https://classroom.google.com/c/NTg3ODM0NTU2MDMy')
--- Python/Piramide.py
+++
@@ -1,6 +0,0 @@
-n = int(input('Escolha um numero: '))
-
-for c in range(n + 1):
-    for p in range(c):
-        print(p, end=' ')
-    print()
