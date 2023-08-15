100f2b4d093b3b48ae78ff33c0c2307470ad6b94
Tigasexe <gabrielizaias173@gmail.com>
2023-08-14 21:41:01 -0300

    Painel de Login em andamente!

--- loucuras/painelLogin/script.js
+++ loucuras/painelLogin/script.js
@@ -1,33 +1,43 @@
-var txt = document.getElementsByClassName('resultado')[0]
+let txt = document.getElementsByClassName('resultado')[0]
 txt.style.opacity = '0'
 
 
 function registrou() {
-    var usuarioreg = document.getElementById('usuario-reg').value
-    var senhareg = document.getElementById('senha-reg').value
-    localStorage = usuarioreg
-    localStorage = senhareg
+    let usuarioreg = document.getElementById('usuario-reg').value
+    let senhareg = document.getElementById('senha-reg').value
+    localStorage.user = usuarioreg
+    localStorage.pass = senhareg
+
+
     if (usuarioreg.length == 0 || senhareg.length == 0) {
-        window.alert('Preencha todos os dados corretamente!')
-    } else {
         txt.style.opacity = '100'
-        txt.style.color = '#FFD700'
+        txt.style.color = 'red'
+        txt.style.fontSize = '14pt'
+        txt.innerHTML = 'Preencha todos os dados corretamente!'
+    } else if (usuarioreg.length <= 5){
+        txt.style.opacity = '100'
+        txt.style.color = 'red'
+        txt.style.fontSize = '14pt'
+        txt.innerHTML = `O usuario ${usuarioreg} não possui 5 ou mais caracteres!`
+    } else {
+        window.location.href = '../index.html'
     }
 }
 
-
 function logar() {
-    var usuario = document.getElementById('usuario').value
-    var senha = document.getElementById('senha').value
+    let usuario = document.getElementById('usuario').value
+    let senha = document.getElementById('senha').value
 
-    if (usuario.length == 0 || senha.length == 0){
-        window.alert('Preencha todos os dados corretamente!')
-    } else if (usuario == localStorage.usuarioreg && senha == localStorage.senhareg) {
+
+    if (usuario != localStorage.user || senha != localStorage.pass) {
         txt.style.opacity = '100'
-        txt.style.color = '#FFD700'
-    } else if (usuario == "Owner" && senha == "owner") {
+        txt.style.color = 'red'
+        txt.style.fontSize = '14pt'
+        txt.innerHTML = 'Usuario ou senha incorretos!'
+    } else {
         txt.style.opacity = '100'
-        txt.style.color = '#FFD700'
-        txt.innerHTML = `&check; Seja bem bindo Chefinho!`
+        txt.style.color = 'green'
+        txt.style.fontSize = '14pt'
+        txt.innerHTML = 'DEU CERTO!'
     }
 }
